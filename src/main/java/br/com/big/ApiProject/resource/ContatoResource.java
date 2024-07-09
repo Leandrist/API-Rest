f44c03cf41a492be14550d1f4801ce73f9773bdd
Leandro Fabrício <goesleandro15@gmail.com>
2024-07-09 06:11:03 -0300

    Quarto Commit - ContatoService

---
+++ src/main/java/br/com/big/ApiProject/resource/ContatoResource.java
@@ -0,0 +1,5 @@
+package br.com.big.ApiProject.resource;
+
+public class ContatoResource {
+
+}
--- src/main/java/br/com/big/ApiProject/service/ContatoService.java
+++ src/main/java/br/com/big/ApiProject/service/ContatoService.java
@@ -1,20 +1,19 @@
 package br.com.big.ApiProject.service;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.big.ApiProject.model.Contato;
 import br.com.big.ApiProject.model.Pessoa;
+import br.com.big.ApiProject.model.TipoContato;
 import br.com.big.ApiProject.repository.ContatoRepository;
 import br.com.big.ApiProject.repository.PessoaRepository;
-import br.com.big.ApiProject.service.interfaces.ContatoServiceInterface;
-
 
 @Service
-public class ContatoService implements ContatoServiceInterface {
+public class ContatoService {
 
 	@Autowired
 	private ContatoRepository contatoRepository;
@@ -22,26 +21,9 @@ public class ContatoService implements ContatoServiceInterface {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
-	@Override
-	public List<Contato> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Optional<Contato> findById() {
-		// TODO Auto-generated method stub
-		return Optional.empty();
-	}
-
-	@Override
-	public void delete(long id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public Contato save(Contato contato) {
+		validateContato(contato);
+		
 		if(contato.getPessoa().getId() != null) {
 			
 			Optional<Pessoa> findPessoa = pessoaRepository.
@@ -62,24 +44,71 @@ public class ContatoService implements ContatoServiceInterface {
 		}
 	}
 
-	@Override
-	public Contato update(Contato contato) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public Optional<Contato> findById(Long id) {
+		return contatoRepository.findById(id); 
 	}
 	
+	public List<Contato> findAll() {
+		return contatoRepository.findAll(); 
+	}
+
+	public void delete(long id) {
+		contatoRepository.deleteById(id);
+	}
+
+
+	public Contato update(Contato contato) {
+		validateContato(contato);
+		
+		Optional<Contato> findContato = contatoRepository.findById(contato.getId());
+		
+		if(findContato.isPresent()) {
+			Contato updContato = findContato.get();
+			updContato.setContato(contato.getContato());
+			updContato.setPessoa(contato.getPessoa());
+			updContato.setTipoContato(contato.getTipoContato());
+
+			return contatoRepository.save(updContato); //UPDATE
+		}else {
+			//INSERT
+			return save(contato);
+		}
+		
+	}
 	
-	
-	//Regras de validação de número
+
+    private void validateContato(Contato contato) {
+        if (contato.getTipoContato() == null) {
+            throw new IllegalArgumentException("TipoContato não pode ser nulo.");
+        }
+
+        if (contato.getPessoa() == null) {
+            throw new IllegalArgumentException("Pessoa não pode ser nula.");
+        }
+
+        if (contato.getContato() == null || contato.getContato().isEmpty()) {
+            throw new IllegalArgumentException("Contato não pode ser nulo ou vazio.");
+        }
+
+        if (contato.getTipoContato() == TipoContato.Cel && !isCelularValido(contato.getContato())) {
+            throw new IllegalArgumentException("Número de celular inválido. Deve conter 11 dígitos numéricos.");
+        }
+
+        if (contato.getTipoContato() == TipoContato.Tel && !isTelefoneValido(contato.getContato())) {
+            throw new IllegalArgumentException("Número de telefone inválido. Deve conter 10 dígitos numéricos.");
+        }
+    }
+
     private boolean isCelularValido(String numero) {
         // Tem 11 dígitos numéricos?
         return numero.matches("\\d{11}");
     }
+
     private boolean isTelefoneValido(String numero) {
         // Tem 10 dígitos numéricos?
         return numero.matches("\\d{10}");
     }
-
 }
 		
 	
